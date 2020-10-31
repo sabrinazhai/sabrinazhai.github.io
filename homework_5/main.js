a653a2eccabfa754d62cd5f09ccb6ad29611c95a
sabrina <svz1999@gmail.com>
2020-10-30 22:05:26 -0400

    clean up

--- homework_5/main.js
+++ homework_5/main.js
@@ -1,52 +1,51 @@
 var cartArray = [];
 
-    window.onload = function(){
-        const colorsButtons = document.getElementsByClassName("colors");
-        const fillButtons = document.getElementsByClassName("fill");
-        const image = document.getElementById("product-photo");
+/* Changing selection and images */
+window.onload = function(){
+    const colorsButtons = document.getElementsByClassName("colors");
+    const fillButtons = document.getElementsByClassName("fill");
+    const image = document.getElementById("product-photo");
 
-        for (let i = 0; i < colorsButtons.length; i++) {
-            let btn = colorsButtons[i];
+    for (let i = 0; i < colorsButtons.length; i++) {
+        let btn = colorsButtons[i];
 
-            btn.addEventListener('click', function(){
-                document.querySelector(".colors.selected").classList.remove('selected');
-                this.classList.add("selected");
-    
-                image.src = "img/photo" + this.dataset.name + ".png";
-            });
-        }
+        btn.addEventListener('click', function(){
+            document.querySelector(".colors.selected").classList.remove('selected');
+            this.classList.add("selected");
 
-        for (let i = 0; i < fillButtons.length; i++) {
-            let btn = fillButtons[i];
-
-            btn.addEventListener('click', function(){
-                document.querySelector(".fill.selected").classList.remove('selected');
-                this.classList.add("selected");
-            });
-        }
-
-
+            image.src = "img/photo" + this.dataset.name + ".png";
+        });
     }
 
-    function addToCart(){
-        var colorValue = document.querySelector(".colors.selected").innerHTML; 
-        var fillValue = document.querySelector(".fill.selected").innerHTML; 
-        var currentItem = {
-            color: colorValue,
-            fill: fillValue
-        }
-        cartArray.push(currentItem);
-        console.log("currently:" + cartArray.length)
+    for (let i = 0; i < fillButtons.length; i++) {
+        let btn = fillButtons[i];
 
-        if (cartArray.length > 0){
-            document.getElementById("cart").innerHTML = cartArray.length + " cart";
-        }
-
+        btn.addEventListener('click', function(){
+            document.querySelector(".fill.selected").classList.remove('selected');
+            this.classList.add("selected");
+        });
     }
+}
 
-    function myFunction() {
-      var x = document.getElementById("navigationbar");
-      if (x.className === "navbar") {
+/* Saving item to the cart into an array */ 
+function addToCart(){
+    var colorValue = document.querySelector(".colors.selected").innerHTML; 
+    var fillValue = document.querySelector(".fill.selected").innerHTML; 
+    var currentItem = {
+        color: colorValue,
+        fill: fillValue
+    }
+    cartArray.push(currentItem);
+    console.log("currently:" + cartArray.length)
+
+    if (cartArray.length > 0){
+        document.getElementById("cart").innerHTML = cartArray.length + " cart";
+    }
+}
+
+function myFunction() {
+    var x = document.getElementById("navigationbar");
+    if (x.className === "navbar") {
         x.className += " responsive";
     } else {
         x.className = "navbar";
